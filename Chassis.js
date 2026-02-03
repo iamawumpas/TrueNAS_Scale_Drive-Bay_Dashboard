dfd7ac498ba132e2aa1c40042e3d5cc26170eb34
iamawumpas <130360693+iamawumpas@users.noreply.github.com>
2026-02-03 08:00:39 +0000

    reverted legend layout changes

--- Chassis.js
+++ Chassis.js
@@ -11,20 +11,21 @@ export function createChassisHTML(pci, data) {
                         <span class="pci-address-grey">${pciRaw}</span>
                     </div>
                 </div>
+
+                <div class="legend-box">
+                    <div class="legend-items-row">
+                        <div class="legend-item"><span class="dot green"></span> ALLOCATED-HEALTHY</div>
+                        <div class="legend-item"><span class="dot allocated-offline"></span> ALLOCATED-OFFLINE</div>
+                        <div class="legend-item"><span class="dot orange"></span> ERROR</div>
+                        <div class="legend-item"><span class="dot red"></span> FAULTED</div>
+                        <div class="legend-item"><span class="dot white"></span> RESILVERING</div>
+                        <div class="legend-item"><span class="dot purple"></span> UNALLOCATED</div>
+                        <div class="legend-item"><span class="dot unalloc-error"></span> UNALLOC-ERROR</div>
+                        <div class="legend-item"><span class="dot unalloc-fault"></span> UNALLOC-FAULT</div>
+                    </div>
+                </div>
             </div>
             <div class="slots" id="slots-${pci}"></div>
-        </div>
-        <div class="legend-box">
-            <div class="legend-items-row">
-                <div class="legend-item"><span class="dot green"></span> ALLOCATED-HEALTHY</div>
-                <div class="legend-item"><span class="dot allocated-offline"></span> ALLOCATED-OFFLINE</div>
-                <div class="legend-item"><span class="dot orange"></span> ERROR</div>
-                <div class="legend-item"><span class="dot red"></span> FAULTED</div>
-                <div class="legend-item"><span class="dot white"></span> RESILVERING</div>
-                <div class="legend-item"><span class="dot purple"></span> UNALLOCATED</div>
-                <div class="legend-item"><span class="dot unalloc-error"></span> UNALLOC-ERROR</div>
-                <div class="legend-item"><span class="dot unalloc-fault"></span> UNALLOC-FAULT</div>
-            </div>
         </div>
     `;
 }
